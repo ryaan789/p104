a7cf0b6bcc856c8e02d2fa350418802bb62ae139
ryaan789 <96716792+ryaan789@users.noreply.github.com>
2022-09-06 14:54:29 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+Webcam.set({ 
+    width:350,
+    height:300,
+    image_format:'png',
+    png_quality:100
+    });
+    
+    Webcam.attach('#camera'); 
+    
+    function take_snapshot()
+    {
+        Webcam.snap(function(data_uri) {
+            document.getElementById("result").innerHTML='<img id="captured_image" src="'+data_uri+'">';
+        });
+    }
+    
+    classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/uZASI_KUH/model.json',modelLoaded);
+    
+    function modelLoaded(){
+        console.log('Model Loaded!');
+    }
+    
+    
